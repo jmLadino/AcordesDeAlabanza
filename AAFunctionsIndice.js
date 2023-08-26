bf710779b7f842c0db516e0be9012bc1ab2870a9
“jmLadino” <josemiguel.ladino@gmail.com>
2023-08-25 20:11:51 -0400

    Se agregan los Estilos para la mejora del Indice

---
+++ AAFunctionsIndice.js
@@ -0,0 +1,60 @@
+        // Función para filtrar elementos por letra
+        document.querySelectorAll('.letra').forEach(function(boton) {
+            boton.addEventListener('click', function() {
+                const letra = boton.textContent;
+                filtrarPorLetra(letra);
+            });
+        });
+
+        // Función para filtrar elementos por letra
+        function filtrarPorLetra(letra) {
+            const elementos = document.querySelectorAll('.texto-centro');
+            elementos.forEach(function(elemento) {
+			
+				if (letra.toUpperCase() === "TODOS")
+				{
+					elemento.parentNode.style.display = 'block';
+				}
+				else
+				{
+					var primeraLetra = elemento.textContent.charAt(0).toUpperCase();
+					if (primeraLetra == "Á") primeraLetra = "A";
+					if (primeraLetra == "É") primeraLetra = "E";
+					if (primeraLetra == "Í") primeraLetra = "I";
+					if (primeraLetra == "Ó") primeraLetra = "O";
+					if (primeraLetra == "Ú") primeraLetra = "U";
+					if (primeraLetra == "¿") primeraLetra = elemento.textContent.charAt(1).toUpperCase();;
+					
+					if (primeraLetra === letra.toUpperCase()) {
+						elemento.parentNode.style.display = 'block';
+					} else {
+						elemento.parentNode.style.display = 'none';
+					}
+				}
+            });
+        }
+
+        // Función para buscar elementos por texto
+        document.querySelector('#buscar').addEventListener('keyup', function() {
+            const texto = this.value.toLowerCase();
+            const elementos = document.querySelectorAll('.texto-centro');
+            elementos.forEach(function(elemento) {
+                const contenido = elemento.textContent.toLowerCase();
+                if (contenido.includes(texto)) {
+                    elemento.parentNode.style.display = 'block';
+                } else {
+                    elemento.parentNode.style.display = 'none';
+                }
+            });
+        });
+		
+		
+        // Función para alternar la visibilidad de las secciones
+        function toggleSection(id) {
+            var elemento = document.getElementById(id);
+            if (elemento.style.display === "" || elemento.style.display === "block") {
+                elemento.style.display = "none";
+            } else {
+                elemento.style.display = "block";
+            }
+        }		
